17edb65bcd75e1001424bb090080af82c3ed5eac
VinayReddyTera <teravinay97@gmail.com>
2025-03-10 18:36:28 +0530

    added console.log

--- index.js
+++ index.js
@@ -5,18 +5,9 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.json());
-const fs = require('fs');
-
-app.use((req, res, next) => {
-  fs.appendFile('log.txt', JSON.stringify(req.body) + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  next();
-});
 
 app.post('/get-salesforce-response', (req, res) => {
+    console.log(req.body);
     res.json(
         {
             "status": 200,
